650ea31e5ed94634e717bf02c42786314cab28be
dmitrypanteleeff <dmitrypanteleeff@yandex.ru>
2022-01-25 02:19:24 +0300

    gh-pages add

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ function Footer() {
     <div className="footer-copyright">
       <div className="container">
       © {new Date().getFullYear()} Copyright Text
-      <a className="grey-text text-lighten-4 right" href="#!">More Links</a>
+      <a className="grey-text text-lighten-4 right" href="https://github.com/dmitrypanteleeff/react-shop" target="_blank" rel="noreferrer">Repo</a>
       </div>
     </div>
   </footer>
